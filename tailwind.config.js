7b98576abad5a8498c6863cdd136766ee2d6b3c8
HaydenRic <63795142+HaydenRic@users.noreply.github.com>
2025-06-09 08:13:37 +0100

    Apply Archivo and Assistant fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,11 @@ export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
+      fontFamily: {
+        'heading': ['Archivo', 'sans-serif'],
+        'body': ['Assistant', 'sans-serif'],
+        'sans': ['Assistant', 'sans-serif'],
+      },
       animation: {
         spotlight: "spotlight 2s ease .75s 1 forwards",
       },
